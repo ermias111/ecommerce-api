05d12cf071726bc51a83eaed4fcdc5bd24d016a9
ermias111 <ermimichael@gmail.com>
2020-11-02 23:13:44 +0300

    task 1 & 2

--- server/models/item.js
+++ server/models/item.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function(Item) {
-
+    Item.beforeRemote('find', function(ctx, unused, next) {
+        if (ctx.args.filter) {
+            if (ctx.args.filter.order) {
+                next()
+            } else {
+                ctx.args.filter.order = 'price DESC';
+                next()
+            }
+        } else {
+            ctx.args.filter = {
+                order: 'price DESC'
+            }
+            next()
+        }
+      });
 };
